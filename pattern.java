9847309f4434d074bfd6ca600a773abcd1bae321
mishramann <piyushmishra08oct@gmail.com>
2025-08-31 07:53:27 +0530

    pattern

---
+++ pattern.java
@@ -0,0 +1,29 @@
+public class pattern {
+  public static void main(String[] args) {
+    // Printing pattern
+    // 1. pattern
+    // int j;
+    // for (int i = 0; i < 5; i++) {
+    // for (j = 0; j < 5; j++) {
+    // System.out.print("x");
+    // }
+    // System.out.println("");
+    // }
+
+    // 2. pattern
+    // for (int i = 0; i <= 5; i++) {
+    // for (int j = 0; j < i; j++) {
+    // System.out.print("x");
+    // }
+    // System.out.println("");
+
+    // }
+
+    // 3. pattern
+    for(int i = 1; i<=5; i++  ){
+      
+
+    }
+
+  }
+}
