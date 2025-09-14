f2b49d2e926345556b303e70251699688e9cfe1b
Piyush Mishra <piyushmishra08oct@gmail.com>
2025-09-14 04:49:18 +0000

    basics javascript

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144356;
+let accountEmail = "piyush@google.com";
+var accountPassword = "12345";
+accoutCity = "Jaipur";
+// accountId = 1234; //not allowed;
+accountEmail = "pm@pm.com";
+accountPassword = "2121213";
+accoutCity = "Bengalore";
+console.log(accountId);
+console.table( accountId,accountEmail,accountId,accoutCity);
+
+
+
+
+
