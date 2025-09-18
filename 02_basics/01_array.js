5e9ce844288841d2e35478391b251445fad9c81e
Piyush Mishra <piyushmishra08oct@gmail.com>
2025-09-18 05:29:27 +0000

    learn the concept of javascript

---
+++ 02_basics/01_array.js
@@ -0,0 +1,35 @@
+//array
+const myArr = [0,1,2,3,4,5,6]
+console.log(myArr); // [0,1,2,3,4,5,6]
+
+
+const myHeroes = ["shaktimaan","nagraaj"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+  
+
+
+// Array Method
+myArr.push(7);     
+console.log(myArr) //[]
+myArr.push(5);    
+console.log(myArr)//[]
+myArr.pop();      
+console.log(myArr) //[]
+myArr.shift(8);
+console.log(myArr) // []
+myArr.shift(2);
+console.log(myArr) // [2,3,4,5,6,7]
+myArr.unshift();
+console.log(myArr) // [2,3,4,5,6,7]
+
+
+console.log(myArr.includes(5)) //5
+console.log(myArr.indexOf(3)); 
+console.log(myArr);
+
+
+
+
+
