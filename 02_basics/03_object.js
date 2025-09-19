bbb3adb20922f7ea31db8ff8700298a4fc8cad30
Piyush Mishra <piyushmishra08oct@gmail.com>
2025-09-19 14:26:13 +0000

    lear js

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -5,10 +5,16 @@
 // object -> and another one way to create an Object is:
 
 //Object Literal
+// ek symbol lo use key me daalo or print karke dhikha dho
+
+
+const mySum = Symbol("key1")
 
 const jsUser = {
    name : "Piyush",
+   "full name" : "Piyush",  //now we have no way to access this without sqare notation 
    age : 20,
+   [mySum] : "mykey1", // if you want to store as symbol one way you have those are square bracket if we wants to access only you access from square bracket
    location:"Haridwar",
    email: "piyushmishra08oct@gmail.com",
    isLoggedIn : false,
@@ -18,6 +24,25 @@ const jsUser = {
 
 } 
 
+// we can acces the object in two ways
+// console.log(jsUser.email)
+// console.log(jsUser["email"])
 
-console.log(jsUser.email)
-console.log(jsUser[email])
+
+// console.log(["full name"])
+// console.log( jsUser[mySum])
+
+ jsUser.email = "mishrapiyush7788@gmail.com"
+//  Object.freeze(jsUser)
+ jsUser.email ="mishrapiyush3409@gmail.com"
+ console.log(jsUser)
+
+
+ jsUser.greeting = function(){
+    console.log("Hello JS user");
+ }
+jsUser.greetingTwo = function(){
+   console.log(`Hello JS user, ${this.name}`)
+}
+console.log(jsUser.greeting()) // Hello JS user
+console.log(jsUser.greetingTwo()) // Hello JS user, Piyush
