6dc91016c6450463190f2ce699b7eecb18be319d
mishramann <piyushmishra08oct@gmail.com>
2025-09-25 23:03:48 +0530

    learn js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,7 @@
 // let a = 10
 // const b = 20
-var c = 300
-// { }            
+// var c = 300     -> this is global scope which is in outside the class
+// { }            -> Scope
 if(true){
   let a = 10
   const b = 20
@@ -9,4 +9,44 @@ if(true){
 }            // curly braces ia always scope   -> inside the function whatever we declare it's called local scope  and declare outside the function is called global scope
 // console.log(a)
 // console.log(b)
-console.log(c)   //its not print the global value of global variable
+// console.log(c)   //its not print the global value of global variable
+ 
+  // block next global scope ?
+
+function one (){
+  const username = "Piyush"
+
+  function two(){
+    const website = "youtube"
+    console.log(username);  //-> inside ka function bahar ke variable ko access kar pata h -> isiko hm closure bhi kehte h
+  }
+  //  console.log(website);  // code yaha pr line by line execute hota h to yahi error dhe dhiya aage chala hi ni
+  // why we can't access this outside the function
+
+  two()
+}
+// one()
+
+if(true){
+  const username = "Piyush Mishra"
+  if(username === "Piyush Mishra"){
+    const website = " youtube"
+    // console.log(username + website);
+  }
+  // console.log(website);   // why conditional statement not execute outside the function ? is function only access within the scope
+}
+// console.log(username);
+
+
+//                         +++++++++++++++++++++++Interesting+++++++++++++++++++++++++++++++++
+
+
+function addOne(num){
+  return num +1
+}
+//  console.log(addOne(5))
+
+const addTwo = function(num){
+  return num +2
+}     
+addTwo(6) // hoisting -> function kese store hota h kaha use rakha jaata h kese use execute kara jaata h kese variable ko javascript treat karti h
---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,32 @@
+const user = {
+  username : "Piyush",
+  price:999,
+
+  welcomeMessage: function() {
+       console.log(`${this.username} ,  welcome to website`);
+      //  console.log(this)
+  }
+    //  Arrow function me this keyword nahi hota h what the reason behind it
+}
+// user.welcomeMessage()
+// user.username = 'sam'
+// user.welcomeMessage()
+//  console.log(this)   // in node envirement this refer to empty string
+
+//  function chai(){ 
+//   let username = "piyush"
+//   console.log(this.username);
+//  }
+//  chai()
+const chai =  () => {
+  let usename = "piyush"
+  console.log(this.username);
+}
+const add1Two = (num1,num2) => {
+  return num1 +num2            // explicitor in that we use return keyword
+}     // => this is the Arrow function
+const addTwo = (num1 , num2 ) => (num1 +num2 )    //   this is implicitor         // we can write also in this format 
+// in this what is going there is while wite in curly bracket it neccesary to write return keyword if we write within the paranthesis there no need to use return keyword
+
+
+console.log(addTwo(3,4));   
