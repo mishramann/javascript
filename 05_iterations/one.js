9f4b55c49b1fafa1b287298f64d68077eabd1d46
Piyush Mishra <piyushmishra08oct@gmail.com>
2025-09-28 15:52:58 +0530

    learn js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -14,7 +14,7 @@
 // }
 let myArray = ["flash", "batman", "superman"]
 for (let index = 0; index < myArray.length; index++) {
-  const element = myArray[index];
+  const element = myArray[index]; 
   // console.log(element);
   
 }
