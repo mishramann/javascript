6ff7ce919bb15a8399773b415102a4bf226eff4f
mishramann <piyushmishra08oct@gmail.com>
2025-09-28 15:49:39 +0530

    learn js

--- 04_control_flow/truelly.js
+++ 04_control_flow/truelly.js
@@ -20,3 +20,23 @@ const emptyObj = {}
 if(Object.keys(emptyObj).length  === 0){
   console.log("Object is empty");
 } // Object is empty
+
+// false == 0 ; true;
+// false == '' ;  true;
+// 0 == '' ; true;
+ 
+// Nullish Coalescing Operator(??) : null undefined
+  let val1 ;
+  val1 = 5 ?? 10
+  val1 = null ?? 10
+  val1 = undefined ?? 15
+  val1 = null ?? 10 ?? 20
+ console.log(val1);
+
+ //terniary Operator
+ let n = 10;
+// if  (n == 0) ? console.log("Hello") :console.log();
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("Less than 80") : console.log("more than 80");
---
+++ 05_iterations/one.js
@@ -0,0 +1,22 @@
+// for
+// for (let i = 0; i < 10; i++) {
+//   const element = i;
+//   if(element == 5){
+//     // console.log("5 is best number");
+//   }
+// }  // 5 is best number
+// for (let i = 0; i <= 10; i++) {
+// console.log(`Outer loop value : ${i}`);
+// for(let j =0 ; j<=10; j++){
+//  // console.log(`Inner loop value ${j} and inner loop ${i}`);
+//  console.log(i + '*' + j + '=' + i*j);
+// }
+// }
+let myArray = ["flash", "batman", "superman"]
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  // console.log(element);
+  
+}
+
+// 
