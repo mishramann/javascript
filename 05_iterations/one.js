8bc2f89ecde33e7aee2f1397e06371f12b451f76
mishramann <piyushmishra08oct@gmail.com>
2025-10-06 09:16:57 +0530

    learn js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -13,10 +13,39 @@
 // }
 // }
 let myArray = ["flash", "batman", "superman"]
-for (let index = 0; index < myArray.length; index++) {
+for (let index = 0; index < myArray.length; index++) {    // if we write only index here so only fleah is ewpeate unlimited time
   const element = myArray[index];
   // console.log(element);
-  
+
+   
 }
 
-// 
+// break and continue
+for (let index = 1; index <= 20; index++) {
+  if(index == 5){
+    console.log("Detected 5");
+    // break; // after using break what is happen that iteration after the break keyword can stop
+    continue
+  }
+  console.log(`value of i is ${index}`); 
+// value of i is 1
+// value of i is 2
+// value of i is 3
+// value of i is 4
+// value of i is 5
+// value of i is 6
+// value of i is 7
+// value of i is 8
+// value of i is 9
+// value of i is 10
+// value of i is 11
+// value of i is 12
+// value of i is 13
+// value of i is 14
+// value of i is 15  
+// value of i is 16
+// value of i is 17
+// value of i is 18
+// value of i is 19
+// value of i is 20
+}
